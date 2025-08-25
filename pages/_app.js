700ee798b6b9079e3a443aeb23690c852e86be35
Aaron Joyce <aaron.joyce@datadoghq.com>
2025-08-25 15:00:42 +0100

    Removing datadog config

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import '@/styles/globals.css'
-import DatadogInit from '@/components/datadog-init'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <DatadogInit />
       <Component {...pageProps} />
     </>
   )
